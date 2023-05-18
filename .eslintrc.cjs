bc484248bbd3a73388aacf5155d5d4723ddd4b4d
delwerhossain <delwerhossain006@gmail.com>
2023-05-18 15:57:50 +0100

    eslint update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true, es6: true },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
