f1d854bcb0c9250c04e751b5cd6e8a6d0819ce60
delwerhossain <delwerhossain006@gmail.com>
2023-05-19 13:42:57 +0100

    react loading added

---
+++ src/Components/Loading/Loading.jsx
@@ -0,0 +1,14 @@
+import { DotLoader } from "react-spinners";
+
+const Loading = () => {
+  return (
+    <div
+      id="spinner"
+      className="sweet-loading h-screen flex justify-center items-center"
+    >
+      <DotLoader color="#600cfc" loading size={250} speedMultiplier={2} />
+    </div>
+  );
+};
+
+export default Loading;
