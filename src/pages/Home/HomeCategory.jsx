1f1891244d675bd5428cd3f726b43fb4da54990e
Delwer Hossain <delwerhossain006@gmail.com>
2023-05-19 07:10:40 +0600

    naming fix

--- src/pages/Home/HomeCategory.jsx
+++ src/pages/Home/HomeCategory.jsx
@@ -25,43 +25,55 @@ const HomeCategory = () => {
       </h1>
       <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 items-center justify-items-center ">
         <div className="border p-4 rounded-xl mb-6 bg-[#FFF8EA] text-center text-3xl ">
-          <h2 className="my-4 text-black">Art & Books</h2>
+          <h2 className="my-4 text-yellow-950 border rounded-3xl w-1/2 border-yellow-700 bg-yellow-50 font-bold">Heroes Toys</h2>
           <div className="grid gap-8">
             {herosCategory.slice(0, 2).map((card) => (
               <Card key={card._id} card={card}></Card>
             ))}
-            
-                      <button className="btn bg-yellow-950 text-slate-50 font-bold text-2xl"> See all </button>
+
+            <button className="btn bg-yellow-950 text-slate-50 font-bold text-2xl">
+              {" "}
+              See all{" "}
+            </button>
           </div>
         </div>
         <div className="border p-4 rounded-xl mb-6 bg-[#FFF8EA] text-center text-3xl ">
-          <h2 className="my-4 text-black">Art & Books</h2>
+          <h2 className="my-4 text-yellow-950 border rounded-3xl w-1/2 border-yellow-700 bg-yellow-50 font-bold">Art & Books</h2>
           <div className="grid gap-8">
             {artCategory.slice(0, 2).map((card) => (
               <Card key={card._id} card={card}></Card>
             ))}
-            
-                      <button className="btn bg-yellow-950 text-slate-50 font-bold text-2xl"> See all </button>
+
+            <button className="btn bg-yellow-950 text-slate-50 font-bold text-2xl">
+              {" "}
+              See all{" "}
+            </button>
           </div>
         </div>
         <div className="border p-4 rounded-xl mb-6 bg-[#FFF8EA] text-center text-3xl ">
-          <h2 className="my-4 text-black">Art & Books</h2>
+          <h2 className="my-4 text-yellow-950 border rounded-3xl w-1/2 border-yellow-700 bg-yellow-50 font-bold">Art & Books</h2>
           <div className="grid gap-8">
             {essentialsCategory.slice(0, 2).map((card) => (
               <Card key={card._id} card={card}></Card>
             ))}
-            
-                      <button className="btn bg-yellow-950 text-slate-50 font-bold text-2xl"> See all </button>
+
+            <button className="btn bg-yellow-950 text-slate-50 font-bold text-2xl">
+              {" "}
+              See all{" "}
+            </button>
           </div>
         </div>
         <div className="border p-4 rounded-xl mb-6 bg-[#FFF8EA] text-center text-3xl ">
-          <h2 className="my-4 text-black">Art & Books</h2>
+          <h2 className="my-4 text-yellow-950 border rounded-3xl w-1/2 border-yellow-700 bg-yellow-50 font-bold">Art & Books</h2>
           <div className="grid gap-8">
             {artBookCategory.slice(0, 2).map((card) => (
               <Card key={card._id} card={card}></Card>
             ))}
-            
-                      <button className="btn bg-yellow-950 text-slate-50 font-bold text-2xl"> See all </button>
+
+            <button className="btn bg-yellow-950 text-slate-50 font-bold text-2xl">
+              {" "}
+              See all{" "}
+            </button>
           </div>
         </div>
       </div>
