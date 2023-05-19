a32817349d1d52fe571a6c82cd7c94d5264d0760
Delwer Hossain <delwerhossain006@gmail.com>
2023-05-19 08:48:00 +0600

    conditional render for title

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -5,17 +5,21 @@ import CateCard from "./CateCard";
 const Category = () => {
   const cateID = useLoaderData();
 
-
+console.log(cateID);
   return (
     <div>
-      <h1 className="text-6xl text-red-600">this is categories</h1>
+      {!(cateID.length > 15) && (
+        <h1 className="md:text-6xl text-4xl text-primary text-center my-8 font-bold underline">
+          Category - {cateID.slice(0,1).map((product) => product.subCategory)}
+        </h1>
+      )}
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <h2 className="sr-only">Products</h2>
 
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3  xl:gap-x-8">
             {cateID.map((product) => (
-             <CateCard key={product._id} product={product}></CateCard>
+              <CateCard key={product._id} product={product}></CateCard>
             ))}
           </div>
         </div>
