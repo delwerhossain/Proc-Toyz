3c4ac5dceb07aec6874b8a67c8c88d864cdba555
Delwer Hossain <delwerhossain006@gmail.com>
2023-05-21 13:02:23 +0600

    name fix

--- src/pages/ProductUpsert/ProductUpsert.jsx
+++ src/pages/ProductUpsert/ProductUpsert.jsx
@@ -234,7 +234,7 @@ const ProductUpsert = () => {
             htmlFor="description"
             className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Email address
+            Toys Description
           </label>
         </div>
         <button
