34cac3c8345c1cdafe06cd596f3859224b8b2192
Delwer Hossain <delwerhossain006@gmail.com>
2023-05-21 22:15:35 +0600

    error img aded

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -13,15 +13,19 @@ const ErrorPage = () => {
             Sorry, we couldn’t find the page you’re looking for.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <Link  
-              to={'/'}
+            <Link
+              to={"/"}
               className="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
             >
               Go back home
-            </Link >
-         
+            </Link>
           </div>
         </div>
+          <img
+            className="h-screen mt-6"
+            src="https://img.freepik.com/free-vector/glitch-error-404-page_23-2148105404.jpg"
+            alt=""
+          />
       </main>
     );
 };
